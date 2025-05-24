93b46485be152e0975dab11a54fed46ef2da399e
DanielFeldman1 <daniel.feldman303@gmail.com>
2025-05-24 19:45:23 +0300

    Updated About Me section
    [+] Tweaked about me section to not only be text, added some svgs, still WIP.

--- src/app/page.js
+++ src/app/page.js
@@ -220,14 +220,20 @@ export default function Portfolio() {
               <h2 className="text-5xl font-semibold mb-2 bg-gradient-to-r text-transparent from-cyan-400 via-purple-500 to-orange-400 bg-clip-text">
                 Hi, I'm Daniel Feldman.
               </h2>
-              <p className="text-2xl text-gray-600 max-w-xl">
-                Software Engineering Student passionate about machine learning,
-                full stack development, and elegant digital solutions.
+              <p className="text-2xl text-gray-600 max-w-xl mb-2 font-normal">
+                Software Engineering Student, passionate about building clean
+                and intelligent applications.
+              </p>
+              <p className="text-lg text-gray-500 max-w-xl mb-2">
+                UI/UX Design | Full-Stack Development | Machine Learning
+              </p>
+              <p className="text-md text-gray-400 max-w-xl m-2 italic">
+                Open to internships and collaborative projects in 2025.
               </p>
               <div className="pt-4 grid grid-cols-2 gap-4">
                 <a
                   href="https://linkedin.com/in/dany-feldman"
-                  className="flex items-center flex-col text-gray-300 hover:text-cyan-400 duration-500"
+                  className="flex items-center flex-col text-gray-400 hover:text-cyan-400 duration-500"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -243,7 +249,7 @@ export default function Portfolio() {
                 </a>
                 <a
                   href="https://github.com/DanielFeldman1"
-                  className="flex flex-col items-center text-gray-300 hover:text-purple-400 duration-500"
+                  className="flex flex-col items-center text-gray-400 hover:text-purple-400 duration-500"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -258,6 +264,9 @@ export default function Portfolio() {
                   GitHub
                 </a>
               </div>
+              <a href="#projects" className="mt-2 hover:text-orange-400">
+                View my work.
+              </a>
             </div>
           </section>
 
@@ -267,18 +276,66 @@ export default function Portfolio() {
               About Me
             </h3>
             <div className="bg-white rounded-lg shadow-lg px-8 py-8">
+              <h4 className="font-semibold text-2xl">
+                Braude College of Engineering
+              </h4>
+              <div className="space-y-2 my-2">
+                <div className="flex flex-row gap-3 items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="text-yellow-500 bi bi-trophy-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5q0 .807-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33 33 0 0 1 2.5.5m.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935m10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935" />
+                  </svg>
+                  <p>Dean's Excellence List (1 Year).</p>
+                </div>
+                <div className="flex flex-row gap-3 items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-pen-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001" />
+                  </svg>
+                  <p>Cumulative GPA: 86.</p>
+                </div>
+              </div>
               <p className="text-lg text-gray-600 mb-3">
-                I'm a Software Engineering student at Braude College of
-                Engineering, with a current GPA of 86 and two semesters on the
-                Dean's List. I'm passionate about full-stack web development,
-                machine learning, and building real-world apps that deliver
-                value.
+                I’ve worked with React, Next.js, Python, MongoDB, and Firebase,
+                and I thrive in collaborative environments where solving
+                real-world problems is the goal. I'm always learning and pushing
+                my limits whether it's through code or new ideas.
               </p>
-              <p className="text-lg text-gray-600">
-                I have hands-on experience in React, Next.js, Python, MongoDB,
-                and Firebase. I enjoy solving tough problems, working in teams,
-                and constantly pushing myself to learn more.
+              <h4 className="font-semibold text-2xl">
+                Personal Life
+              </h4>
+              <p className="text-lg text-gray-600 italic">
+                Outside the editor, I’m training for a half marathon and love
+                reading books on tech, mindset, and personal growth. Weight training, Mindfulness. I believe
+                consistent effort, in code or life, leads to powerful outcomes.
               </p>
+              <div className='mt-2'>
+                <div className='flex flex-row items-center gap-2'>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="text-yellow-800 bi bi-book-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783" />
+                  </svg>
+                  <p>Reading.</p>
+                </div>
+              </div>
             </div>
           </section>
 
@@ -335,13 +392,15 @@ export default function Portfolio() {
                   <p className="mb-3 text-lg text-gray-600">
                     {proj.description}
                   </p>
-                  {proj.github && <a
-                    href={proj.github}
-                    onClick={(e) => e.stopPropagation()}
-                    className="hover:text-purple-400 duration-500 text-md font-semibold"
-                  >
-                    View on GitHub.
-                  </a>}
+                  {proj.github && (
+                    <a
+                      href={proj.github}
+                      onClick={(e) => e.stopPropagation()}
+                      className="hover:text-purple-400 duration-500 text-md font-semibold"
+                    >
+                      View on GitHub.
+                    </a>
+                  )}
                 </div>
               ))}
             </div>
